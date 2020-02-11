beec2e085eac91a62882af437308d3bdc41a8a47
Jonathan LEI <xJonathan@outlook.com>
2020-02-11 21:12:53 +0800

    Initial commit

---
+++ Ipset.NET.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using Ipset;
+
+namespace Ipset.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
